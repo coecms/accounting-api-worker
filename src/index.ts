d6485e1643edbc0b41b489a08d4c4bf16d1e86b0
Dale Roberts <dsroberts@unimelb.edu.au>
2023-10-23 15:39:25 +1100

    Format with prettier

--- src/index.ts
+++ src/index.ts
@@ -8,33 +8,32 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-const allowedHosts = [ 'http://localhost:5173', 'https://climate-cms.org' ];
-const api_server = "https://clex-cms-accounting.azurewebsites.net/api/";
+const allowedHosts = ['http://localhost:5173', 'https://climate-cms.org'];
+const api_server = 'https://clex-cms-accounting.azurewebsites.net/api/';
 
 const handler: ExportedHandler = {
 	async fetch(request: Request) {
+		const url = new URL(request.url);
 
-		const url = new URL(request.url)
-
-		if(request.method.toUpperCase() === 'OPTIONS') {
-			let responseHeaders = setCorsHeaders(new Headers(),request.headers.get('Origin'), url.pathname);
-			return new Response(null, {headers:responseHeaders,status:204});
-		} else if ( url.pathname.startsWith("/v0/auth") ) {
+		if (request.method.toUpperCase() === 'OPTIONS') {
+			let responseHeaders = setCorsHeaders(new Headers(), request.headers.get('Origin'), url.pathname);
+			return new Response(null, { headers: responseHeaders, status: 204 });
+		} else if (url.pathname.startsWith('/v0/auth')) {
 			// Handle auth
 			const init = {
 				method: request.method,
 				headers: request.headers,
-				body: await request.arrayBuffer()
-			}
-			const newRequest = new Request(api_server + url.pathname,init);
+				body: await request.arrayBuffer(),
+			};
+			const newRequest = new Request(api_server + url.pathname, init);
 			const response = await fetch(newRequest);
 			return response;
-		} else if ( request.method.toUpperCase() !== 'GET' ) {
-			return new Response(null,{status:405});
+		} else if (request.method.toUpperCase() !== 'GET') {
+			return new Response(null, { status: 405 });
 		}
 
-		if ( request.headers.get("Authorization") == null ) {
-			return new Response(null,{status:401})
+		if (request.headers.get('Authorization') == null) {
+			return new Response(null, { status: 401 });
 		}
 
 		// Check auth
@@ -44,11 +43,12 @@ const handler: ExportedHandler = {
 			cf: {
 				cacheEverything: true,
 				// Don't expect these to change much
-				cacheTtl: 604800
-			}
-		}
-		const auth_response = await fetch(api_server + '/v0/checkauth?key='+ request.headers.get('Authorization'),auth_init)
-		if ( auth_response.status != 204 ) {
+				// 7 * 24 * 3600 seconds
+				cacheTtl: 604800,
+			},
+		};
+		const auth_response = await fetch(api_server + '/v0/checkauth?key=' + request.headers.get('Authorization'), auth_init);
+		if (auth_response.status != 204) {
 			return auth_response;
 		}
 
@@ -59,13 +59,13 @@ const handler: ExportedHandler = {
 		const outdate = new Date();
 
 		// Round hours to next entry in roundTimes
-		if ( now.getUTCMinutes() >= 30 || !roundTimes.includes(now.getUTCHours()) ) {
-			var outhour = Math.floor((((now.getUTCHours()+5)%24)+24)%24/6)*6+1;
+		if (now.getUTCMinutes() >= 30 || !roundTimes.includes(now.getUTCHours())) {
+			var outhour = Math.floor(((((now.getUTCHours() + 5) % 24) + 24) % 24) / 6) * 6 + 1;
 		} else {
 			outhour = now.getUTCHours();
 		}
-		if ( outhour <= 1  && now.getUTCHours() >= 19 ) {
-			outdate.setUTCDate(outdate.getUTCDate()+1);
+		if (outhour <= 1 && now.getUTCHours() >= 19) {
+			outdate.setUTCDate(outdate.getUTCDate() + 1);
 		}
 		outdate.setUTCHours(outhour);
 		outdate.setUTCMinutes(30);
@@ -76,30 +76,29 @@ const handler: ExportedHandler = {
 			headers: request.headers,
 			cf: {
 				cacheEverything: true,
-				cacheTtl: Math.floor(( outdate.getTime() - now.getTime())/1000)
-			 }
-		}
-		const response = await fetch(api_server + url.pathname + url.search,init);
+				cacheTtl: Math.floor((outdate.getTime() - now.getTime()) / 1000),
+			},
+		};
+		const response = await fetch(api_server + url.pathname + url.search, init);
 		return response;
-	}
+	},
+};
 
-}
-
-function setCorsHeaders(headers: Headers, hostname: string|null, resource: string) {
+function setCorsHeaders(headers: Headers, hostname: string | null, resource: string) {
 	/// Requests into this worker are allowed from two places
-	if ( !hostname ) {
+	if (!hostname) {
 		return headers;
 	}
-	if ( allowedHosts.includes(hostname) ) {
+	if (allowedHosts.includes(hostname)) {
 		headers.set('Access-Control-Allow-Origin', hostname);
-		if ( resource.startsWith("/v0/auth") ) {
+		if (resource.startsWith('/v0/auth')) {
 			headers.set('Access-Control-Allow-Methods', 'POST');
-			headers.set('Access-Control-Allow-Headers','Content-Range, Content-Type, Content-Length');
+			headers.set('Access-Control-Allow-Headers', 'Content-Range, Content-Type, Content-Length');
 		} else {
 			headers.set('Access-Control-Allow-Methods', 'GET');
 			headers.set('Access-Control-Allow-Headers', 'Content-Range, Content-Type, Content-Length, Range, Authorization');
 		}
-		headers.set('Access-Control-Expose-Headers','Content-Range');
+		headers.set('Access-Control-Expose-Headers', 'Content-Range');
 		headers.set('Access-Control-Max-Age', '86400');
 	}
 	return headers;
